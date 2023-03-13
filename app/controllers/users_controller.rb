22982860c4623a4f06782402f279bf9cdedb5e75
Thảo GiBi <125585308+ThaoGiBiBehemoth@users.noreply.github.com>
2023-03-13 17:18:43 +0700

    6th: action_create_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  # SIGNUP/ REGISTER
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({ user_id: @user.id})
+      render json: { user: @user, token: token }, status: 200 
+    else
+      render json: @user.errors, status: 422
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :password, :email)
+  end
 end
